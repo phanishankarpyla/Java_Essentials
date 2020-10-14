e3f36919b7d492287535481caff06878148d0f94
phanishankarpyla <70090876+phanishankarpyla@users.noreply.github.com>
2020-10-15 00:24:15 +0530

    Add files via upload

---
+++ AssignmentDay2/Employee.java
@@ -0,0 +1,14 @@
+package com.LetsUpgrade;
+
+public class Employee {
+    String name;
+    int age;
+    String city;
+
+    public void Printing(){
+        System.out.println("The name is "+name);
+        System.out.println("The age is "+age);
+        System.out.println("The city is "+city+"\n");
+    }
+
+}
---
+++ AssignmentDay2/Main.java
@@ -0,0 +1,17 @@
+package com.LetsUpgrade;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Employee use1= new Employee();
+	Employee use2= new Employee();
+	use1.name="Phani Shankar";
+	use1.age=22;
+	use1.city="Vizag";
+	use2.name="Alekhya";
+	use2.age=23;
+	use2.city="Hyd";
+	use1.Printing();
+	use2.Printing();
+    }
+}
